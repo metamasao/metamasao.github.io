6c791bdebb56f279edaa5bd8c6ca358a0a4f19fe
metamasao <metamasao@gmail.com>
2023-06-14 21:09:43 +0900

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center bg-dark text-dark-emphasis py-1">
-      <p className="my-1">&copy;metamasao</p>
-    </footer>
-  );
-}
--- src/components/Loader.js
+++
@@ -1,7 +0,0 @@
-import { RingLoader } from "react-spinners";
-
-export default function Loader() {
-  return (
-    <RingLoader cssOverride={{margin: "15px auto"}} size={150}/>
-  )
-}
