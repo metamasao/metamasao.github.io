6e088fda5ee90d137c9ebb284e5c8ad2435e02a3
metamasao <metamasao@gmail.com>
2023-06-16 15:19:31 +0900

    using HashRouter instead of Browser

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import BlogProvider from './components/BlogProvider';
 
