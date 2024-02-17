74740514cebf668cf01642685cde31630ed74103
metamasao <metamasao@gmail.com>
2024-02-17 19:36:24 +0900

    just beginning...

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer({ author }) {
-  return (
-    <footer className="text-center bg-dark text-dark-emphasis py-1 mt-3">
-      <p className="my-1">&copy;{author}</p>
-    </footer>
-  );
-}
